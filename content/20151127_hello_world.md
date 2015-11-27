a7c6389a5e5c5b9107be1cbcbd2af014b5d005a5
Jeff Wen <jeffwen@users.noreply.github.com>
2015-11-27 05:10:05 -0800

    Start blog

---
+++ content/20151127_hello_world.md
@@ -0,0 +1,14 @@
+Title: Hello World!
+Date: 2015-11-27 03:15
+Author: Jeff Wen
+Category: Misc
+Slug: hello-world
+Summary: First blog post
+
+
+Hello everyone, my name is Jeff. I've started this blog to keep track of the things that I am up to and also to share about exiciting things that I am learning. Thanks for stopping by!
+
+
+```python
+print 'Hello World!'
+```
---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jeff Wen'
+SITENAME = u'Jeff Wen'
+SITEURL = 'http://jeffwen.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+DEFAULT_DATE_FORMAT = ('%B %d %Y')
+
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}'
+
+ARCHIVES_SAVE_AS = 'archives.html'
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
+
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
